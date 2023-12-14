fc10006190dea2438801172a0c06b72a4d117d42
faizanhasan <mdfaizanalam367@gmail.com>
2023-12-15 01:15:16 +0530

    form validation

---
+++ src/utils/Validate.jsx
@@ -0,0 +1,10 @@
+export const checkValidData = (email, password) => {
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+
+    const isPasswordValid = /^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/.test(password);
+
+    if (!isEmailValid) return "Email ID is Not Valid";
+    if (!isPasswordValid) return "Password is Not Valid";
+
+    return null;
+};
